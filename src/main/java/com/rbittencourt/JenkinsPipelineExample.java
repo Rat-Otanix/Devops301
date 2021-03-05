490876abf1d2c87c0d685a49f9863f02ab9d0fd6
Ratnesh chaturvedi <ratneshc84@gmail.com>
2021-03-05 19:08:10 +0530

    Update JenkinsPipelineExample.java

--- src/main/java/com/rbittencourt/JenkinsPipelineExample.java
+++ src/main/java/com/rbittencourt/JenkinsPipelineExample.java
@@ -2,9 +2,10 @@ package com.rbittencourt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsPipelineExample {
+public class JenkinsPipelineExample extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsPipelineExample.class, args);
